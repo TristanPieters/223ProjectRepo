41353dcf5b4b2c974933c64f85f6ec63daefe260
Dewald <dewald13.dw@gmail.com>
2019-09-08 20:04:32 +0200

    Auto stash before merge of "master" and "origin/master"

--- CMPG213 Prototype/CMPG213 Prototype/AccountUpdateDel.cs
+++ CMPG213 Prototype/CMPG213 Prototype/AccountUpdateDel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CMPG213_Prototype
 {
@@ -16,12 +17,46 @@ namespace CMPG213_Prototype
         {
             InitializeComponent();
         }
+        SqlConnection CONN = new SqlConnection();
+        SqlDataAdapter adapter;
+
 
         private void AccountUpdateDel_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'stallionsDbDataSet.ACCOUNT' table. You can move, or remove it, as needed.
             this.aCCOUNTTableAdapter.Fill(this.stallionsDbDataSet.ACCOUNT);
 
+            CONN = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Dewald\Desktop\CMPG223\223ProjectRepo\CMPG213 Prototype\CMPG213 Prototype\StallionsDb.mdf;Integrated Security=True");
+        }
+
+        private void BtnDeleteAccount_Click(object sender, EventArgs e)
+        {
+       /*     con.Open();
+            SqlCommand command;
+            string sql;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            sql = @" DELETE FROM Account WHERE LastName ='" + textBox1.Text + "'"";
+ command = new SqlCommand(sql, con);
+            DataSet ds = new DataSet();
+            adapter.DeleteCommand = command;
+            adapter.DeleteCommand.ExecuteNonQuery(); //Deletes from database, Adapter
+            makes changes
+
+ con.Close();*/
+        }
+
+        private void TbxID_TextChanged(object sender, EventArgs e)
+        {
+           
+            
+
+            string query = @"SELECT * from ACCOUNT WHERE Acc_IDnum
+ LIKE '%" + tbxID.Text + "%'";
+ SqlDataAdapter adapter = new SqlDataAdapter(query, CONN);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "ACCOUNT");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "ACCOUNT";
         }
     }
 }
--- CMPG213 Prototype/CMPG213 Prototype/MaintainFuel.Designer.cs
+++ CMPG213 Prototype/CMPG213 Prototype/MaintainFuel.Designer.cs
@@ -62,10 +62,18 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
+<<<<<<< Updated upstream
             this.tabControl1.Location = new System.Drawing.Point(19, 37);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(320, 186);
+=======
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(450, 313);
+>>>>>>> Stashed changes
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -76,21 +84,32 @@
             this.tabPage1.Controls.Add(this.textBox1);
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(8, 39);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tabPage1.Name = "tabPage1";
+<<<<<<< Updated upstream
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(312, 160);
+=======
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tabPage1.Size = new System.Drawing.Size(434, 266);
+>>>>>>> Stashed changes
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Add";
             this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
             // 
             // button1
             // 
+<<<<<<< Updated upstream
             this.button1.BackColor = System.Drawing.Color.White;
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.Location = new System.Drawing.Point(9, 69);
+=======
+            this.button1.Location = new System.Drawing.Point(18, 133);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+>>>>>>> Stashed changes
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(150, 44);
             this.button1.TabIndex = 4;
             this.button1.Text = "ADD";
             this.button1.UseVisualStyleBackColor = false;
@@ -98,33 +117,45 @@
             // 
             // textBox2
             // 
+<<<<<<< Updated upstream
             this.textBox2.Location = new System.Drawing.Point(152, 39);
+=======
+            this.textBox2.Location = new System.Drawing.Point(214, 67);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+>>>>>>> Stashed changes
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(196, 31);
             this.textBox2.TabIndex = 3;
             // 
             // textBox1
             // 
+<<<<<<< Updated upstream
             this.textBox1.Location = new System.Drawing.Point(152, 11);
+=======
+            this.textBox1.Location = new System.Drawing.Point(214, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+>>>>>>> Stashed changes
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(196, 31);
             this.textBox1.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 38);
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.Size = new System.Drawing.Size(152, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Price per Liter:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 11);
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.Size = new System.Drawing.Size(174, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Fuel Description:";
             // 
@@ -134,37 +165,76 @@
             this.tabPage2.Controls.Add(this.comboBox2);
             this.tabPage2.Controls.Add(this.button2);
             this.tabPage2.Controls.Add(this.label4);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(8, 39);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tabPage2.Name = "tabPage2";
+<<<<<<< Updated upstream
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(312, 160);
+=======
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tabPage2.Size = new System.Drawing.Size(434, 266);
+>>>>>>> Stashed changes
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Delete";
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
+<<<<<<< Updated upstream
             this.comboBox2.Location = new System.Drawing.Point(139, 12);
+=======
+            this.comboBox2.Location = new System.Drawing.Point(218, 23);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+>>>>>>> Stashed changes
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(100, 21);
+            this.comboBox2.Size = new System.Drawing.Size(196, 33);
             this.comboBox2.TabIndex = 12;
             // 
             // button2
             // 
+<<<<<<< Updated upstream
             this.button2.Location = new System.Drawing.Point(8, 47);
+=======
+            this.button2.Location = new System.Drawing.Point(22, 140);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+>>>>>>> Stashed changes
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(150, 44);
             this.button2.TabIndex = 9;
             this.button2.Text = "Delete";
             this.button2.UseVisualStyleBackColor = true;
+<<<<<<< Updated upstream
             this.button2.Click += new System.EventHandler(this.button2_Click);
+=======
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(218, 75);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(196, 31);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 81);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Price per Liter:";
+>>>>>>> Stashed changes
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(8, 15);
+            this.label4.Location = new System.Drawing.Point(16, 29);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.Size = new System.Drawing.Size(174, 25);
             this.label4.TabIndex = 5;
             this.label4.Text = "Fuel Description:";
             // 
@@ -178,69 +248,97 @@
             this.tabPage3.Controls.Add(this.tbFR);
             this.tabPage3.Controls.Add(this.label5);
             this.tabPage3.Controls.Add(this.label6);
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Location = new System.Drawing.Point(8, 39);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tabPage3.Name = "tabPage3";
+<<<<<<< Updated upstream
             this.tabPage3.Size = new System.Drawing.Size(312, 160);
+=======
+            this.tabPage3.Size = new System.Drawing.Size(434, 266);
+>>>>>>> Stashed changes
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Update";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
+<<<<<<< Updated upstream
             this.comboBox1.Location = new System.Drawing.Point(139, 11);
+=======
+            this.comboBox1.Location = new System.Drawing.Point(218, 21);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+>>>>>>> Stashed changes
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.Size = new System.Drawing.Size(196, 33);
             this.comboBox1.TabIndex = 11;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(8, 68);
+            this.label7.Location = new System.Drawing.Point(16, 131);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.Size = new System.Drawing.Size(157, 25);
             this.label7.TabIndex = 10;
             this.label7.Text = "Fuel Reserves:";
             // 
             // button3
             // 
+<<<<<<< Updated upstream
             this.button3.BackColor = System.Drawing.Color.White;
             this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
             this.button3.Location = new System.Drawing.Point(8, 99);
+=======
+            this.button3.Location = new System.Drawing.Point(16, 190);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+>>>>>>> Stashed changes
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(150, 44);
             this.button3.TabIndex = 9;
             this.button3.Text = "Update";
             this.button3.UseVisualStyleBackColor = false;
             // 
             // tbPPL
             // 
+<<<<<<< Updated upstream
             this.tbPPL.Location = new System.Drawing.Point(139, 41);
+=======
+            this.tbPPL.Location = new System.Drawing.Point(218, 73);
+            this.tbPPL.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+>>>>>>> Stashed changes
             this.tbPPL.Name = "tbPPL";
-            this.tbPPL.Size = new System.Drawing.Size(100, 20);
+            this.tbPPL.Size = new System.Drawing.Size(196, 31);
             this.tbPPL.TabIndex = 8;
             // 
             // tbFR
             // 
+<<<<<<< Updated upstream
             this.tbFR.Location = new System.Drawing.Point(139, 69);
+=======
+            this.tbFR.Location = new System.Drawing.Point(218, 125);
+            this.tbFR.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+>>>>>>> Stashed changes
             this.tbFR.Name = "tbFR";
-            this.tbFR.Size = new System.Drawing.Size(100, 20);
+            this.tbFR.Size = new System.Drawing.Size(196, 31);
             this.tbFR.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(8, 41);
+            this.label5.Location = new System.Drawing.Point(16, 79);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.Size = new System.Drawing.Size(152, 25);
             this.label5.TabIndex = 6;
             this.label5.Text = "Price per Liter:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(8, 14);
+            this.label6.Location = new System.Drawing.Point(16, 27);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.Size = new System.Drawing.Size(174, 25);
             this.label6.TabIndex = 5;
             this.label6.Text = "Fuel Description:";
             // 
@@ -268,10 +366,15 @@
             // 
             // MaintainFuel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+<<<<<<< Updated upstream
             this.ClientSize = new System.Drawing.Size(394, 251);
+=======
+            this.ClientSize = new System.Drawing.Size(486, 327);
+>>>>>>> Stashed changes
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "MaintainFuel";
             this.Text = "MaintainFuel";
             this.Load += new System.EventHandler(this.MaintainFuel_Load);
